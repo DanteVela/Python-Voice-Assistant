456ba5a38d99b60fd82860335942071a3fe2f34c
DanteVela <70284605+DanteVela@users.noreply.github.com>
2025-06-18 18:28:15 -0500

    Prototype AI

---
+++ VoiceAssistant.py
@@ -0,0 +1,98 @@
+# Speech Recognition is the process of turning spoken words into text. It is a key part of any voice assistant. 
+# In Python, the Speech Recognition module helps us do this by capturing audio and converting it to text.
+
+# Run the following command in the command prompt to install all necessary libraries:
+# pip install SpeechRecognition pyttsx3 wikipedia pyjokes   (Python)
+# !pip install SpeechRecognition pyttsx3 wikipedia pyjokes  (Jupyter Notebook)
+
+# To run the assistant, simply call "python [Filename].py" by using the "cmd Terminal".
+
+"""Import all the libraries needed: (pyttsx3) for speaking out loud, (datetime) to fetch the current time, 
+(wikipedia) to search and fetch content, including many more."""
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import pyjokes
+
+# (Speak) function that accepts a string as input. This function prints and speaks input.
+def speak(text):
+    print(f"Assistant: {text}")
+    try:
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+    except:
+        print("Speech output not supported in Colab.")
+
+
+# A wish_user() function to greet the user based on the current time:
+# - If it’s before 12 PM, it says “Good Morning”.
+# - If it’s before 6 PM, it says “Good Afternoon”.
+# - Otherwise it says “Good Evening”.
+
+# This makes the assistant feel more natural and friendly. It also introduces itself and asks how it can help.
+def wish_user():
+    hour = int(datetime.datetime.now().hour)
+    if hour < 12:
+        speak("Good Morning!")
+    elif hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+    speak("I am your voice assistant. How can I help you today?")
+
+# Normally you would talk to the assistant using your microphone. 
+# But since Google Colab doesn’t support voice input we use this by creating a list of commands in code:
+# - "what is python wikipedia"
+# - "open youtube"
+# - "what's the time"
+# - "exit"
+
+# This list helps us test how the assistant will respond without needing a real voice input.
+def take_command():
+    return input("You (type your command): ").lower()
+
+# The (run_assistant) function loops through the list of commands and checks what each command is asking for.
+def run_assistant():
+    wish_user()
+    while True:
+        query = take_command()
+
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            try:
+                result = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia:")
+                speak(result)
+            except:
+                speak("Sorry, I couldn't find anything.")
+
+        elif 'open youtube' in query:
+            speak("Opening YouTube...")
+            webbrowser.open("https://www.youtube.com")
+
+        elif 'open google' in query:
+            speak("Opening Google...")
+            webbrowser.open("https://www.google.com")
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The current time is {strTime}")
+
+        elif 'joke' in query:
+            joke = pyjokes.get_joke()
+            speak(joke)
+
+        elif 'exit' in query or 'bye' in query:
+            speak("Goodbye! Have a nice day!")
+            break
+
+        else:
+            speak("Sorry, I didn't understand that. Try again.")
+
+# This function starts the assistant and keeps it running until the user says "exit" or "bye".
+run_assistant()
